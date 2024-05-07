95a1c78d99c5b183f9179f2c100e4a391fc882b8
Bradley Hastings <bradleybeasley2000@gmail.com>
2024-05-07 11:10:14 +0100

    Minor Tweaks

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,8 +3,7 @@ import UserDropdown from "./user-dropdown"
 const Header = () => {
   return (
     <header className="flex flex-row flex-nowrap justify-between p-4">
-      <h1 className="text-4xl font-semibold">Darkest Depths</h1>
-      <UserDropdown />
+      <UserDropdown className="ml-auto"/>
     </header>
   )
 }
