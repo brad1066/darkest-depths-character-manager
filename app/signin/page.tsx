8fbac5e282aeef20b89bf2d28799e90965fe0b3a
Bradley Hastings <bradleybeasley2000@gmail.com>
2024-09-19 12:13:19 +0100

    various adjustments

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,26 +1,40 @@
+'use server'
+
 import { redirect } from 'next/navigation'
 import { signIn } from '@/lib/auth'
 import { AuthError } from 'next-auth'
 import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { FaGoogle } from 'react-icons/fa'
 
 export default async function SignInPage() {
   return (
-    <div className="flex flex-col gap-2">
-      <form
-        action={async () => {
-          'use server'
-          try {
-            await signIn('google')
-          } catch (error) {
-            if (error instanceof AuthError) {
-              return redirect(`/api/auth/signin&error=${error.type}`)
+    <Card className="m-auto w-min-[40rem] w-[50%]">
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+        <CardDescription>Sign in with one of these options:</CardDescription>
+      </CardHeader>
+      <CardContent>
+      </CardContent>
+      <CardFooter>
+        <form
+          action={async () => {
+            'use server'
+            try {
+              await signIn('google')
+            } catch (error) {
+              if (error instanceof AuthError) {
+                return redirect(`/api/auth/signin&error=${error.type}`)
+              }
+              throw error
             }
-            throw error
-          }
-        }}
-      >
-        <Button type="submit">Sign in with Google</Button>
-      </form>
-    </div>
+          }}
+        >
+          <Button type="submit" variant="outline" size="icon">
+            <FaGoogle />
+          </Button>
+        </form>
+      </CardFooter>
+    </Card>
   )
 }
